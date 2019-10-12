e1fc22ab1fca5fae1f4c1cc75ee686abdb7f1e95
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-10-12 02:11:29 -0300

    Refactor hooks and add lib helpers

---
+++ app/src/lib/math-utils.js
@@ -0,0 +1,8 @@
+export function round(num, places = 2) {
+  const rounded = Number(Math.round(num + 'e+' + places) + 'e-' + places)
+  return Number.isNaN(rounded) ? Number(num.toFixed(places)) : rounded
+}
+
+export function toHours(ms) {
+  return ms / 3600000
+}
