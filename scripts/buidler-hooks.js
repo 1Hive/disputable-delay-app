6365122ff8617916788f15f67dc17f1fdb2fef0a
William Griffiths <willjgriff@gmail.com>
2020-06-08 12:41:02 +0100

    Migrate to builder.

---
+++ scripts/buidler-hooks.js
@@ -0,0 +1,48 @@
+let pct16
+
+let tokens, appManager
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+module.exports = {
+  postDao: async function({ _experimentalAppInstaller, log, dao }, builderRuntimeEnv) {
+    const bigExp = (x, y) =>
+      builderRuntimeEnv.web3.utils
+        .toBN(x)
+        .mul(builderRuntimeEnv.web3.utils.toBN(10).pow(builderRuntimeEnv.web3.utils.toBN(y)))
+    pct16 = (x) => bigExp(x, 16)
+
+    appManager = (await builderRuntimeEnv.web3.eth.getAccounts())[0]
+
+    const minime = await deployMinimeToken(builderRuntimeEnv)
+    await minime.generateTokens(appManager, pct16(100))
+    log(`> Minime token deployed: ${minime.address}`)
+
+    tokens = await _experimentalAppInstaller('token-manager', {
+      skipInitialize: true
+    })
+
+    await minime.changeController(tokens.address)
+    log(`> Change minime controller to tokens app`)
+    await tokens.initialize([minime.address, true, 0])
+    log(`> Tokens app installed: ${tokens.address}`)
+  },
+
+  getInitParams: async function({}, builderRuntimeEnv) {
+    return [
+      60 // Delay time in seconds
+    ]
+  },
+
+  postInit: async function({ proxy }, bre) {
+    await tokens.createPermission('MINT_ROLE', proxy.address)
+  }
+}
+
+const deployMinimeToken = async (bre) => {
+  const MiniMeTokenFactory = await bre.artifacts.require('MiniMeTokenFactory')
+  const MiniMeToken = await bre.artifacts.require('MiniMeToken')
+  const factory = await MiniMeTokenFactory.new()
+  return await MiniMeToken.new(factory.address, ZERO_ADDRESS, 0, 'MiniMe Test Token', 18, 'MMT', true
+  )
+}
